3db54ac88a650088db4513d38e59924323556e69
strangefish <strangefish2000@gmail.com>
2020-06-09 13:18:09 +0100

    npm installed mongoose and body-parser. Added mongoose setup to app.js

--- app.js
+++ app.js
@@ -3,16 +3,33 @@
 !    localhost:3000
 */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+//mongoose setup
+mongoose.connect('mongodb://localhost:27017/bhh', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+// Check connection
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+// Check for db errors
+db.on('error', function (err) {
+  console.error(err);
+});
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
